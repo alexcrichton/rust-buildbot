95b97e6c2cf60ef2f62064c8220f69e38b5b47d7
Nathan Froyd <froydnj@gmail.com>
2016-04-22 10:51:01 -0400

    use API level 9 for arm and x86
    This change makes them more compatible with Firefox releases, which
    currently use API level 9 as their baseline.
    
    Fixes #98.

--- slaves/android/install-ndk.sh
+++ slaves/android/install-ndk.sh
@@ -8,10 +8,13 @@ set -ex
 curl -O http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin
 chmod +x android-ndk-r10e-linux-x86_64.bin
 ./android-ndk-r10e-linux-x86_64.bin > /dev/null
+# We use android-9 on arm and x86 for maximum compatibility with systems
+# like Firefox.  We use 21 for aarch64 and x86-64 because that is the
+# earliest API level at which they are supported.
 bash android-ndk-r10e/build/tools/make-standalone-toolchain.sh \
-        --platform=android-18 \
+        --platform=android-9 \
         --toolchain=arm-linux-androideabi-4.8 \
-        --install-dir=/android/ndk-arm-18 \
+        --install-dir=/android/ndk-arm-9 \
         --ndk-dir=/android/android-ndk-r10e \
         --arch=arm
 bash android-ndk-r10e/build/tools/make-standalone-toolchain.sh \
@@ -27,7 +30,7 @@ bash android-ndk-r10e/build/tools/make-standalone-toolchain.sh \
         --ndk-dir=/android/android-ndk-r10e \
         --arch=arm64
 bash android-ndk-r10e/build/tools/make-standalone-toolchain.sh \
-        --platform=android-21 \
+        --platform=android-9 \
         --toolchain=x86-4.9 \
         --install-dir=/android/ndk-x86 \
         --ndk-dir=/android/android-ndk-r10e \
